18e85e356789a2582e35327dfa1d07b05a316c7f
Matheus Silva <matheuscarvas@gmail.com>
2018-08-13 23:01:42 -0300

    read key if exists

--- constants.py
+++ constants.py
@@ -1,6 +1,11 @@
-with open('./data/api_key.txt', 'r') as f:
-    API_KEY = f.read()
+import os
 
-API_URL = 'https://www.alphavantage.co/query?function=' \
-          'TIME_SERIES_DAILY_ADJUSTED&symbol={}.SA&apikey={}'\
-    .format('{}', API_KEY)
+API_FILE_KEY = './data/api_key.txt'
+
+if os.path.isfile(API_FILE_KEY):
+    with open(API_FILE_KEY, 'r') as f:
+        API_KEY = f.read()
+
+    API_URL = 'https://www.alphavantage.co/query?function=' \
+              'TIME_SERIES_DAILY_ADJUSTED&symbol={}.SA&apikey={}'\
+        .format('{}', API_KEY)
