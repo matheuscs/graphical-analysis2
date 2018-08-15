a7aad86b21206892287607a441c9538a9f6c6e9e
Matheus Silva <matheuscarvas@gmail.com>
2018-08-15 00:07:22 -0300

    unmocking and using environment variable for storing the key

--- constants.py
+++ constants.py
@@ -1,11 +1,6 @@
 import os
 
-API_FILE_KEY = './data/api_key.txt'
-
-if os.path.isfile(API_FILE_KEY):
-    with open(API_FILE_KEY, 'r') as f:
-        API_KEY = f.read()
-
-    API_URL = 'https://www.alphavantage.co/query?function=' \
-              'TIME_SERIES_DAILY&symbol={}.SA&apikey={}&outputsize=full'\
-        .format('{}', API_KEY)
+API_KEY = os.environ['ALPHAVANTAGE_API_KEY']
+API_URL = 'https://www.alphavantage.co/query?function=' \
+          'TIME_SERIES_DAILY&symbol={}.SA&apikey={}&outputsize=full'\
+    .format('{}', API_KEY)
--- tests/test_finance_api.py
+++ tests/test_finance_api.py
@@ -1,17 +1,9 @@
-from unittest import TestCase, mock
-import json
+from unittest import TestCase
 import finance_api
 
 
-def mocked_make_api_request(_):
-    with open('./data/mocked_request_data.txt', 'r') as f:
-        return json.load(f)
-
-
 class TestFinanceAPI(TestCase):
-    @mock.patch('finance_api.make_api_request',
-                side_effect=mocked_make_api_request)
-    def test_make_api_request(self, _):
+    def test_make_api_request(self):
         fun = finance_api.make_api_request('BBAS3')
         self.assertIn('Meta Data', fun)
         self.assertIn('1. Information', fun['Meta Data'])
