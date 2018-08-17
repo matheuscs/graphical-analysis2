fae8d44ac547b5e2842aabd2dc2138680599540f
Matheus Silva <matheuscarvas@gmail.com>
2018-08-16 21:41:45 -0300

    less calls to the api request

--- tests/test_finance_api.py
+++ tests/test_finance_api.py
@@ -3,22 +3,19 @@ from helpers import finance_api as api
 
 
 class TestFinanceAPI(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.api_request = api.make_api_request('BBAS3')
+
     def test_make_api_request(self):
-        fun = api.make_api_request('BBAS3')
-        self.assertIn('Meta Data', fun)
-        self.assertIn('1. Information', fun['Meta Data'])
+        self.assertIn('Meta Data', self.api_request)
+        self.assertIn('1. Information', self.api_request['Meta Data'])
         self.assertIn('Daily Prices (open, high, low, close) and Volumes',
-                      fun['Meta Data']['1. Information'])
-        self.assertIn('Time Series (Daily)', fun)
+                      self.api_request['Meta Data']['1. Information'])
+        self.assertIn('Time Series (Daily)', self.api_request)
 
     def test_dataframe_from_request_output(self):
         self.assertEqual(
-            len(api.dataframe_from_request_output(
-                api.make_api_request('bbas3'), 20)),
-            20
-        )
+            len(api.dataframe_from_request_output(self.api_request, 20)), 20)
         self.assertEqual(
-            api.dataframe_from_request_output(
-                api.make_api_request('bbas3'), 20).size,
-            100
-        )
+            api.dataframe_from_request_output(self.api_request, 20).size, 100)
